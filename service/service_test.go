6082eb7ac26fce2531d427e6a7640c61c7d8d74a
Steve Streeting <steve@stevestreeting.com>
2018-11-16 16:14:00 +0000

    Generalise upload test and add test for not hard linking

--- service/service_test.go
+++ service/service_test.go
@@ -87,61 +87,27 @@ func finishUpload(buf *bytes.Buffer) {
 }
 
 func TestUpload(t *testing.T) {
-	// Create 2 temporary dirs, pretending to be git repo and dest shared folder
-	gitpath, err := ioutil.TempDir(os.TempDir(), "lfs-folderstore-test-src")
-	assert.Nil(t, err, "Error creating temp git path")
-	defer os.RemoveAll(gitpath)
 
-	storepath, err := ioutil.TempDir(os.TempDir(), "lfs-folderstore-test-dest")
-	assert.Nil(t, err, "Error creating temp shared path")
-	defer os.RemoveAll(storepath)
-
-	testfiles := []struct {
-		path string
-		size int64
-	}{
-
-		{ // small file
-			filepath.Join(gitpath, "file1"),
-			650,
-		},
-		{ // Multiple block file
-			filepath.Join(gitpath, "file2"),
-			4 * 1024 * 16 * 2,
-		},
-		{ // Multiple block file with remainder
-			filepath.Join(gitpath, "file3"),
-			4*1024*16*6 + 345,
-		},
-	}
-
-	oids := make([]string, len(testfiles))
-
-	for i, file := range testfiles {
-		oids[i] = createTestFile(t, file.size, file.path)
-	}
-
-	// Construct an input buffer of commands to upload first 2 files
-	var commandBuf bytes.Buffer
-	initUpload(&commandBuf)
-
-	for i := 0; i < 2; i++ {
-		file := testfiles[i]
-		addUpload(t, &commandBuf, file.path, oids[i], file.size)
-	}
-
-	finishUpload(&commandBuf)
+	setup := setupUploadTest(t)
+	defer os.RemoveAll(setup.localpath)
+	defer os.RemoveAll(setup.remotepath)
 
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 
 	// Perform entire sequence
-	Serve(storepath, bytes.NewReader(commandBuf.Bytes()), &stdout, &stderr)
+	Serve(setup.remotepath, bytes.NewReader(setup.inputBuffer.Bytes()), &stdout, &stderr)
+
+	os.Stdout.WriteString("stdout: ")
+	os.Stdout.Write(stdout.Bytes())
+
+	os.Stdout.WriteString("stderr: ")
+	os.Stdout.Write(stderr.Bytes())
 
 	// Check both files
 	for i := 0; i < 2; i++ {
-		file := testfiles[i]
-		expectedPath := filepath.Join(storepath, oids[i][0:2], oids[i][2:4], oids[i])
+		file := setup.files[i]
+		expectedPath := filepath.Join(setup.remotepath, file.oid[0:2], file.oid[2:4], file.oid)
 		assert.FileExistsf(t, expectedPath, "Store file must exist: %v", expectedPath)
 
 		// Check size of file
@@ -150,7 +116,7 @@ func TestUpload(t *testing.T) {
 
 		// Re-calculate hash to verify
 		oid := calculateFileHash(t, expectedPath)
-		assert.Equal(t, oids[i], oid)
+		assert.Equal(t, file.oid, oid)
 	}
 
 	// Now try to perform an upload with files 2 & 3 - only one is new
@@ -158,9 +124,108 @@ func TestUpload(t *testing.T) {
 
 }
 
+func TestUploadNoLinking(t *testing.T) {
+	// bit of a hack, but force no hard linking to make sure we test real copy
+	disableHardLinking = true
+	defer func() { disableHardLinking = false }()
+
+	setup := setupUploadTest(t)
+	defer os.RemoveAll(setup.localpath)
+	defer os.RemoveAll(setup.remotepath)
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	// Perform entire sequence
+	Serve(setup.remotepath, bytes.NewReader(setup.inputBuffer.Bytes()), &stdout, &stderr)
+
+	os.Stdout.WriteString("stdout: ")
+	os.Stdout.Write(stdout.Bytes())
+
+	os.Stdout.WriteString("stderr: ")
+	os.Stdout.Write(stderr.Bytes())
+
+	// Check both files
+	for i := 0; i < 2; i++ {
+		file := setup.files[i]
+		expectedPath := filepath.Join(setup.remotepath, file.oid[0:2], file.oid[2:4], file.oid)
+		assert.FileExistsf(t, expectedPath, "Store file must exist: %v", expectedPath)
+
+		// Check size of file
+		s, _ := os.Stat(expectedPath)
+		assert.Equal(t, file.size, s.Size())
+
+		// Re-calculate hash to verify
+		oid := calculateFileHash(t, expectedPath)
+		assert.Equal(t, file.oid, oid)
+	}
+
+}
+
+type testFile struct {
+	path string
+	size int64
+	oid  string
+}
+type testSetup struct {
+	localpath   string
+	remotepath  string
+	files       []testFile
+	inputBuffer *bytes.Buffer
+}
+
+func setupUploadTest(t *testing.T) *testSetup {
+	// Create 2 temporary dirs, pretending to be git repo and dest shared folder
+	gitpath, err := ioutil.TempDir(os.TempDir(), "lfs-folderstore-test-src")
+	assert.Nil(t, err, "Error creating temp git path")
+
+	storepath, err := ioutil.TempDir(os.TempDir(), "lfs-folderstore-test-dest")
+	assert.Nil(t, err, "Error creating temp shared path")
+
+	testfiles := []testFile{
+		{ // small file
+			path: filepath.Join(gitpath, "file1"),
+			size: 650,
+		},
+		{ // Multiple block file
+			path: filepath.Join(gitpath, "file2"),
+			size: 4 * 1024 * 16 * 2,
+		},
+		{ // Multiple block file with remainder
+			path: filepath.Join(gitpath, "file3"),
+			size: 4*1024*16*6 + 345,
+		},
+	}
+
+	for i, file := range testfiles {
+		// note must reindex since file is by value
+		testfiles[i].oid = createTestFile(t, file.size, file.path)
+	}
+
+	// Construct an input buffer of commands to upload first 2 files
+	var commandBuf bytes.Buffer
+	initUpload(&commandBuf)
+
+	for i := 0; i < 2; i++ {
+		file := testfiles[i]
+		addUpload(t, &commandBuf, file.path, file.oid, file.size)
+	}
+
+	finishUpload(&commandBuf)
+
+	return &testSetup{
+		localpath:   gitpath,
+		remotepath:  storepath,
+		files:       testfiles,
+		inputBuffer: &commandBuf,
+	}
+
+}
+
 func createTestFile(t *testing.T, size int64, path string) string {
 	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_EXCL, 644)
 	assert.Nil(t, err)
+	defer f.Close()
 
 	byteSnippet := []byte{
 		1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
